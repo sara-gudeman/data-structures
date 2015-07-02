d2c629eb4c3e79bd780060d6319e019801cb8d04
sara-gudeman <sgudeman982@gmail.com>
2015-07-01 20:06:52 -0700

    Prototypal stack--implemented size and push

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackObj = Object.create(stackMethods);
+
+  stackObj.length = 0;
+  stackObj.index = 0;
+
+  return stackObj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	size: function() {
+		return this.length;
+	},
+	push: function(value) {
+		this.length++;
+		this.index++;
+	}
+};
 
 
