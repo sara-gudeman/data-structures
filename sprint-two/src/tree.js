30a071f67ffabda58b021d153e47b16972d887f7
sara-gudeman <sgudeman982@gmail.com>
2015-07-02 11:09:56 -0700

    Trees: extended constructor fcn w/methods, created child arrayx

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,14 +2,22 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  extend(newTree, treeMethods);
 
   return newTree;
 };
 
 
-
+var extend = function(obj) {
+	for (var i = 0; i < arguments.length; i++) {
+		for (var k in arguments[i]) {
+			obj[k] = arguments[i][k];
+		}
+	}
+	return obj;
+};
 
 
 var treeMethods = {};
