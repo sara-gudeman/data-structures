a0c148394f7ccbc189783401dbef0d52d6125beb
sara-gudeman <sgudeman982@gmail.com>
2015-07-01 20:16:01 -0700

    prototypal queue: rough draft, passed first few tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queObj = Object.create(queueMethods);
+  queObj.length = 0;
+  return queObj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	size: function(){
+		return this.length;
+	},
+	enqueue: function(value){
+		this.length++;
+	},
+	dequeue: function(){
+		
+	}
+};
 
 
