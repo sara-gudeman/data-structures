76c1c1b97e40cd01d6ce1a917381d53446da7432
sara-gudeman <sgudeman982@gmail.com>
2015-07-02 20:47:55 -0700

    Graph: completed addNode, removeNode, contains

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,18 +4,34 @@ var Graph = function(){
 };
 
 Graph.prototype.addNode = function(node){
+	this[node] = {
+		val: node,
+		edges: {}
+	}
 };
 
 Graph.prototype.contains = function(node){
+	for(var x in this){
+		if(this[x].val === node){
+			return true;
+		}
+	}
+	return false;
 };
 
 Graph.prototype.removeNode = function(node){
+	delete this[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	console.log("from",fromNode.edges)
+	this[fromNode].edges.contains(toNode);
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	console.log(this);
+	this[fromNode].edges[toNode] = toNode;
+	this[toNode].edges[fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
