3b22fdcbcabb2b94d9ab37e9cc51fe846ba25987
sara-gudeman <sgudeman982@gmail.com>
2015-07-03 11:11:42 -0700

    Graph: completed all requirements, answered complexity

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -24,24 +24,36 @@ Graph.prototype.removeNode = function(node){
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
-	console.log("from",fromNode.edges)
-	this[fromNode].edges.contains(toNode);
+	for(var x in this[fromNode].edges){
+		if(this[fromNode].edges[toNode] === toNode){
+			return true;
+		}
+	}
+	return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-	console.log(this);
 	this[fromNode].edges[toNode] = toNode;
 	this[toNode].edges[fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	delete this[fromNode].edges[toNode];
+	delete this[toNode].edges[fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
+	for (var key in this) {
+		if (typeof this[key] === "object") {
+			cb(key);
+		}
+	};
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ constants: addNode, addEdge, removeEdge, removeNode
+ linear: contains, hasEdge, forEachNode
  */
 
 
