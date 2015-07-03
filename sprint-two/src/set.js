9231156f79bc81ac7467d17ee64e7c120daa8eba
sara-gudeman <sgudeman982@gmail.com>
2015-07-03 11:22:57 -0700

    Set: created array to hold data, completed add, contains

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,22 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+	for (var i = 0; i < this._storage.length; i++) {
+		if (this._storage[i] === item) {
+			return true;
+		}
+	}
+	return false;
 };
 
 setPrototype.remove = function(item){
