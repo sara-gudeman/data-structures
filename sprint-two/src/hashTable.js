6f79702b037b6a287eb414491831587b543a1d64
sara-gudeman <sgudeman982@gmail.com>
2015-07-04 15:58:38 -0700

    Refactored retrieve and insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,25 +6,52 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var value = [];
-  if (this.retrieve(k)) {
+  var retrieve = this.retrieve(k);
+  if (retrieve === undefined || retrieve === null) {
+    value.push(k, v);
+  } else {
     var storedArr = this._storage.get(i);
     var newArr = [k, v];
     value.push(storedArr, newArr);
-  } else {
-    value.push(k, v);
   }
-  
+
   this._storage.set(i, value);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var arr = this._storage.get(i);
-  if(arr){
-    return arr[1];
+  if(arr === undefined || arr === null){
+    return null
+  }
+
+
+  // if there is only one tuple sorted at the index
+  if(typeof arr[0] === "string"){
+    return arr[1]
+  }
+  // return value at index 1
+
+
+  // if there are more than one tuples
+  if(Array.isArray(arr[0])){
+  // iterate over each tuple key,
+      // if key is equal to k
+      // return value at subindex 1
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i][0] === k){
+        return arr[i][1]
+      }
+    }
   }else{
     return null;
   }
+  // else 
+    // two cases:
+      // one: remove has been run, value is null
+      // two: value has never been set (undefined)
+      // either way, return null
+
 };
 
 HashTable.prototype.remove = function(k){
