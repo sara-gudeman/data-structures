e2697d589e8fe9bba87c68648fec231e684b27a6
sara-gudeman <sgudeman982@gmail.com>
2015-07-03 22:00:46 -0700

    Hash table: started remove

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -17,6 +17,8 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(i) = null;
 };
 
 
