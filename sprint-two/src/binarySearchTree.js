5930b308f41dcd0f83c039770c1ad48b596ddea4
sara-gudeman <sgudeman982@gmail.com>
2015-07-04 17:31:57 -0700

    BinaryTree: completed insert, pseudocode contains

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,42 @@
 var BinarySearchTree = function(value){
+	var searchTree = {};
+
+	searchTree.value = value;
+
+	searchTree.insert = function(value) {
+		if (searchTree.value < value) {
+			if (searchTree.right === null) {
+				searchTree.right = BinarySearchTree(value);
+			} else {
+				searchTree.right.insert(value);
+			}
+		} else {
+			if (searchTree.left === null) {
+				searchTree.left = BinarySearchTree(value);
+			} else {
+				searchTree.left.insert(value);
+			}
+		}
+	};
+
+	searchTree.contains = function(val) {
+		// if searchTree value is === value
+		// then value exists
+		// -- return?
+		// else if right is not null & value > searchTree value
+
+			// return search.right.contains
+		// else if left is not null & value < searchTree value
+			//return search.left.contains
+		//else
+		//return false
+	};
+
+	searchTree.depthFirstLog = function(cb){};
+	searchTree.right = null;
+	searchTree.left = null;
+
+	return searchTree;
 };
 
 
